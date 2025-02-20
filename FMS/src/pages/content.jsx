0719ba8394df329c7c94b6be483f566f8e9df330
junaidparveez <junaidparwez@gmail.com>
2025-02-20 21:41:35 +0530

    structure changes

--- FMS/src/pages/content.jsx
+++ FMS/src/pages/content.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Login from '../components/Login';
-import HomePage from '../components/HomePage';
+import Login from './Login';
+import HomePage from './HomePage';
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Register from '../components/Register';
+import Register from './Register';
 
 const Content = () => {
   const appRouter = createBrowserRouter([
